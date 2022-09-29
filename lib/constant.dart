1c269b659b371fe228a5ad716e8f217fcd315da6
Ahmad-Sheikh <ahmadabrarsh@gmail.com>
2022-09-29 15:16:23 +0500

    slider

--- lib/constant.dart
+++ lib/constant.dart
@@ -6,5 +6,11 @@ const kActiveCardColor = Color(0xff1D1E33);
 const kInactiveCardColor = Color(0xff111328);
 const kLabelTextStyle = TextStyle(
   fontSize: 18,
+  fontWeight: FontWeight.bold,
   color: Color(0xff8D8E98),
+);
+const kNumberTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w900,
+  color: Colors.white,
 );
--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,8 @@ import 'container_widget.dart';
 import 'container_content_widget.dart';
 import 'constant.dart';
 
+int heights = 180;
+
 enum Gender {
   male,
   female,
@@ -75,15 +77,50 @@ class _HomeState extends State<Home> {
                     },
                     myColor: kActiveCardColor,
                     containerChild: Column(
-                      children: const [
-                        Icon(FontAwesomeIcons.mars),
-                        SizedBox(
-                          height: 15,
-                        ),
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
                         Text(
                           'Height',
                           style: kLabelTextStyle,
-                        )
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.baseline,
+                          textBaseline: TextBaseline.alphabetic,
+                          children: [
+                            Text(
+                              heights.toString(),
+                              style: kNumberTextStyle,
+                            ),
+                            Text(
+                              "cm",
+                              style: kLabelTextStyle,
+                            )
+                          ],
+                        ),
+                        SliderTheme(
+                          data: SliderTheme.of(context).copyWith(
+                            activeTrackColor: Colors.white,
+                            inactiveTrackColor: Color(0xff8D8E98),
+                            thumbColor: Color(0xffEB1555),
+                            overlayColor: Color(0x29EB1555),
+                            thumbShape: RoundSliderThumbShape(
+                              enabledThumbRadius: 15.0,
+                            ),
+                            overlayShape: RoundSliderOverlayShape(
+                              overlayRadius: 30.0,
+                            ),
+                          ),
+                          child: Slider(
+                              value: heights.toDouble(),
+                              min: 100.0,
+                              max: 250.0,
+                              onChanged: (double newValue) {
+                                setState(() {
+                                  heights = newValue.round();
+                                });
+                              }),
+                        ),
                       ],
                     ),
                   ),
@@ -106,14 +143,7 @@ class _HomeState extends State<Home> {
                         SizedBox(
                           height: 15,
                         ),
-                        Text(
-                          'Male',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                            color: textColor,
-                          ),
-                        )
+                        Text('Male', style: kLabelTextStyle)
                       ],
                     ),
                   ),
@@ -130,14 +160,7 @@ class _HomeState extends State<Home> {
                         SizedBox(
                           height: 15,
                         ),
-                        Text(
-                          'Male',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                            color: textColor,
-                          ),
-                        )
+                        Text('Male', style: kLabelTextStyle)
                       ],
                     ),
                   ),
